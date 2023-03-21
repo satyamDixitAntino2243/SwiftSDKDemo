927c3c3005f0eea21e51dcf98e2d34746719a851
Satyam Dixit <satyam.d@antino.io>
2023-03-21 11:41:24 +0530

    Initial project setup

---
+++ projectDemoSDK/projectDemoSDK/projectDemoSDK.h
@@ -0,0 +1,18 @@
+//
+//  projectDemoSDK.h
+//  projectDemoSDK
+//
+//  Created by satyam dixit on 21/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for projectDemoSDK.
+FOUNDATION_EXPORT double projectDemoSDKVersionNumber;
+
+//! Project version string for projectDemoSDK.
+FOUNDATION_EXPORT const unsigned char projectDemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <projectDemoSDK/PublicHeader.h>
+
+
