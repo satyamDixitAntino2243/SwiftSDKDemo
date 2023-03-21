11866c3e5f2580f4cac1dd11ae7ed063d259ff92
Satyam Dixit <satyam.d@antino.io>
2023-03-21 11:53:26 +0530

    added example file

---
+++ projectDemoSDK/projectDemoSDK/projectDemoSDK.swift
@@ -0,0 +1,22 @@
+//
+//  projectDemoSDK.swift
+//  projectDemoSDK
+//
+//  Created by satyam dixit on 21/03/23.
+//
+
+import Foundation
+
+public final class projectDemoSDK {
+
+    let name = "projectDemoSDK"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
